03124a9da0d4087027c8604c948118f0f0ff237c
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2018-04-06 08:49:03 +0200

    change reference to cf-deployment from local path to abs path in user workspace

--- deployments/gke-cf/run.sh
+++ deployments/gke-cf/run.sh
@@ -4,13 +4,13 @@ set -e
 
 cf_lb_ip=$(kubectl -n bosh get svc cf-ingress -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 
-bosh -n -d cf deploy cf-deployment/cf-deployment.yml \
-  --vars-store creds.yml \
+bosh -n -d cf deploy ~/workspace/cf-deployment/cf-deployment.yml \
+  --vars-store cf-creds.yml \
   -v system_domain=${cf_lb_ip}.sslip.io \
-  -o cf-deployment/operations/use-compiled-releases.yml \
-  -o cf-deployment/operations/experimental/use-bosh-dns.yml \
-  -o cf-deployment/operations/experimental/skip-consul-cell-registrations.yml \
-  -o cf-deployment/operations/experimental/skip-consul-locks.yml \
-  -o cf-deployment/operations/experimental/disable-consul.yml \
-  -o cf-deployment/operations/experimental/use-grootfs.yml \
-  -o local.yml
+  -o ~/workspace/cf-deployment/operations/use-compiled-releases.yml \
+  -o ~/workspace/cf-deployment/operations/experimental/use-bosh-dns.yml \
+  -o ~/workspace/cf-deployment/operations/experimental/skip-consul-cell-registrations.yml \
+  -o ~/workspace/cf-deployment/operations/experimental/skip-consul-locks.yml \
+  -o ~/workspace/cf-deployment/operations/experimental/disable-consul.yml \
+  -o ~/workspace/cf-deployment/operations/experimental/use-grootfs.yml \
+  -o ../gke-cf/local.yml
