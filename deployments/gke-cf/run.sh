8549a9f3e11bc9ffece8d3c9365e260367234694
Sean Keery <skibum55@hotmail.com>
2018-10-05 02:15:23 +0000

    v5 cf deployment

--- deployments/gke-cf/run.sh
+++ deployments/gke-cf/run.sh
@@ -4,4 +4,4 @@ set -e
 
 : "${SYSTEM_DOMAIN:?}"
 
-bosh -n -d cf deploy cf-deployment/cf-deployment.yml   --vars-store creds.yml   -v system_domain=system.gcp.seankeery.com -o local.yml -o cf-deployment/operations/experimental/rootless-containers.yml 
+bosh -n -d cf deploy cf-deployment/cf-deployment.yml --vars-store creds.yml -v system_domain=system.gcp.seankeery.com -o cf-deployment/operations/use-compiled-releases.yml -o cf-deployment/operations/scale-to-one-az.yml -o local.yml 
